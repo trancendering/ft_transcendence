8352a1ffa5756afc520c40854dc794a8a6880cb8
함동균 <dh4m28@gmail.com>
2024-01-24 15:11:23 +0900

    Fix: CORS 에러 해결

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "daphne",
     "test_api",
+    "corsheaders",
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,6 +44,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +57,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3001",
+]
 
 TEMPLATES = [
     {
