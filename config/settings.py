42dfa390471861277ef81bfd8308cef142e09cfc
함동균 <dh4m28@gmail.com>
2024-01-29 12:40:32 +0900

    game 디버그용 임시브랜치입니다 삭제예정

--- config/settings.py
+++ config/settings.py
@@ -82,16 +82,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-import os
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
     }
 }
 
