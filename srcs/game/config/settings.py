4bf3e51545d141711d99cbc48c1ce7c396137eb4
dakang <trumpjqk@daum.net>
2024-01-29 15:04:50 +0900

    Feat: oauth 도커랑 통합, 접속까지 확인

--- srcs/game/config/settings.py
+++ srcs/game/config/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT"),
     }
--- srcs/game/tools/docker-entrypoint.sh
+++ srcs/game/tools/docker-entrypoint.sh
@@ -9,8 +9,11 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -subj "/C=KR/L=Seoul/O=42Seoul/CN=nginx" \
         > /dev/null 2>&1
 
+sleep 20
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
 
 # Execute da
-
 
 daphne -b 0.0.0.0 -e ssl:443:privateKey=/etc/daphne/ssl/daphne.key:certKey=/etc/daphne/ssl/daphne.crt config.asgi:application
