84b3e5d243b6dadd4e693ce44a75e8584161b550
함동균 <dh4m28@gmail.com>
2024-02-01 13:55:09 +0900

    Fix: 필요없는 디버그문 삭제

--- game_module/BaseRoom.py
+++ game_module/BaseRoom.py
@@ -259,7 +259,6 @@ class BaseRoom:
             b_to_e_other_basis = ball_to_edge.basis_translate(
                 norm_velocity, Vector(-norm_velocity.y, norm_velocity.x)
             )
-            print(ball_to_edge, b_to_e_other_basis)
             ball_to_edge_dencity = b_to_e_other_basis.x - math.sqrt(
                 self.BALL_SIZE ** 2 - b_to_e_other_basis.y ** 2
             )
